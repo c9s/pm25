133332bc6b0f83bb4f1c41532920af41e63c2cb8
c9s <yoanlin93@gmail.com>
2015-03-10 13:43:52 +0800

    Add station detail controller

---
+++ applications/PM25/Controller/StationDetailController.php
@@ -0,0 +1,10 @@
+<?php
+namespace PM25\Controller;
+use Phifty\Controller;
+
+class StationDetailController extends Controller
+{
+    public function indexAction() {
+        return $this->render( /* your template file */ );
+    }
+}
