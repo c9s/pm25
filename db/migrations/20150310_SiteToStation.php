dbd3c994137477249b30b38dfd59581fa9b416c6
c9s <yoanlin93@gmail.com>
2015-03-10 12:58:14 +0800

    Add migration script

---
+++ db/migrations/20150310_SiteToStation.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * To upgrade with new schema:
+ *    $this->importSchema(new TestApp\AuthorBookSchema);
+ *
+ * To rename table column:
+ *    $this->renameColumn($table, $columnName, $newColumnName);
+ *
+ * To create index:
+ *    $this->createIndex($table,$indexName,$columnNames);
+ *
+ * To drop index:
+ *    $this->dropIndex($table,$indexName);
+ *
+ * To add a foreign key:
+ *    $this->addForeignKey($table,$columnName,$referenceTable,$referenceColumn = null) 
+ *
+ * To drop table:
+ *    $this->dropTable('authors');
+ */
+
+class SiteToStation_1425963243  extends LazyRecord\Migration\Migration {
+
+    public function upgrade() {
+        $this->executeSql('ALTER TABLE sites RENAME stations');
+    }
+
+    public function downgrade() {
+    
+    }
+
+}
