97dbeec4b86185c12744ce357c8e04c5f7a0ad45
c9s <yoanlin93@gmail.com>
2015-03-13 14:33:18 +0800

    Filter stations where address are null

--- scripts/update_site_geo.php
+++ scripts/update_site_geo.php
@@ -3,6 +3,8 @@ require 'main.php';
 use PM25\Model\StationCollection;
 
 $sites = new StationCollection;
+$sites->where()
+    ->is('address', 'NULL');
 foreach($sites as $site) {
     if (!$site->longitude || !$site->latitude) {
         echo "Updating ", $site->name, ": " . $site->getAddress() . " => ";
