f858ff99efeaef353f8810de6a27650e2f77d842
c9s <yoanlin93@gmail.com>
2015-03-13 13:43:15 +0800

    Add location updater query

--- db/migrations/20150311_AddGeoSpatial.php
+++ db/migrations/20150311_AddGeoSpatial.php
@@ -24,6 +24,7 @@ class AddGeoSpatial_1426056847  extends LazyRecord\Migration\Migration {
 
     public function upgrade() {
         $this->executeSql('ALTER TABLE `stations` ADD COLUMN `location` POINT AFTER `longitude`');
+        $this->executeSql('UPDATE stations s SET location = POINT(s.latitude, s.longitude) WHERE s.latitude IS NOT NULL AND s.longitude IS NOT NULL');
     }
 
     public function downgrade() {
