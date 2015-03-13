4613a8421d852e17c4df898ad7c96bd7fe993986
c9s <yoanlin93@gmail.com>
2015-03-13 16:16:10 +0800

    Add TaiwanEPADataSource

---
+++ applications/PM25/DataSource/TaiwanEPADataSource.php
@@ -0,0 +1,57 @@
+<?php
+namespace PM25\DataSource;
+use PM25\Model\Station;
+use PM25\DataSource\TaiwanEPADataSource;
+use CLIFramework\Logger;
+use LazyRecord\ConnectionManager;
+
+class TaiwanEPADataSource
+{
+
+    const STATION_DETAILS_URL = 'http://opendata.epa.gov.tw/ws/Data/AQXSite/?$orderby=SiteName&$skip=0&$top=1000&format=json';
+
+    public function fetchStationDetails() {
+        return json_decode(file_get_contents(self::STATION_DETAILS_URL), false);
+    }
+
+    public function updateStationDetails() {
+        $details = $this->fetchStationDetails();
+        foreach($details as $siteDetail) {
+            echo "Checking " , $siteDetail->SiteName, "\n";
+            $station = new Station;
+            $ret = $station->createOrUpdate([ 
+                'longitude' => doubleval($siteDetail->TWD97Lon),
+                'latitude' => doubleval($siteDetail->TWD97Lat),
+                'name_en' => $siteDetail->SiteEngName,
+                'address' => $siteDetail->SiteAddress,
+                'area'    => $siteDetail->Township,
+                'rawdata' => yaml_emit($siteDetail),
+                // 'remark'  => [ 'type' => $siteDetail->SiteType ],
+            ], [ 'name' => $siteDetail->SiteName ]);
+            if ($ret->error) {
+                die($ret->message);
+            }
+
+            // If we don't have address_en, we should translate the address to english
+            if (! $station->address_en) {
+                $result = $station->requestGeocode($siteDetail->SiteAddress);
+                // $result[0]->address_components;
+                $englishAddress = $result->results[0]->formatted_address;
+                echo "Updating english address => ", $englishAddress , "\n";
+                $site->update(['address_en' => $englishAddress]);
+            }
+
+            if (! $station->city_en && $station->city) {
+                $result = $station->requestGeocode($station->city . ', ' . $station->country);
+                // $result[0]->address_components;
+                $str = $result->results[0]->address_components[0]->long_name;
+                echo "Updating english city name => ", $str , "\n";
+                $station->update(['city_en' => $str]);
+            }
+        }
+    }
+}
+
+
+
+
--- scripts/update_site_details.php
+++ scripts/update_site_details.php
@@ -1,47 +1,9 @@
 <?php
 require 'main.php';
-
 use PM25\Model\Station;
+use PM25\DataSource\TaiwanEPADataSource;
 use CLIFramework\Logger;
 use LazyRecord\ConnectionManager;
-
 $logger = Logger::getInstance();
-
-$logger->info("Fetching opendata from opendata.epa.gov.tw ...");
-$siteDetails = json_decode(file_get_contents('http://opendata.epa.gov.tw/ws/Data/AQXSite/?$orderby=SiteName&$skip=0&$top=1000&format=json'), false);
-
-foreach($siteDetails as $siteDetail) {
-    $site = new Station;
-    $site->load([ 'name' => $siteDetail->SiteName ]);
-    if ($site->id) {
-        $logger->info("Station {$siteDetail->SiteName} loaded");
-        $site->update([ 
-            'longitude' => doubleval($siteDetail->TWD97Lon),
-            'latitude' => doubleval($siteDetail->TWD97Lat),
-            'name_en' => $siteDetail->SiteEngName,
-            'address' => $siteDetail->SiteAddress,
-            'area'    => $siteDetail->Township,
-            'rawdata' => yaml_emit($siteDetail),
-            // 'remark'  => [ 'type' => $siteDetail->SiteType ],
-        ]);
-    }
-
-    if (! $site->address_en) {
-        $result = $site->requestGeocode($siteDetail->SiteAddress);
-        // $result[0]->address_components;
-        $englishAddress = $result->results[0]->formatted_address;
-        echo "Updating english address => ", $englishAddress , "\n";
-        $site->update(['address_en' => $englishAddress]);
-    }
-
-    if (! $site->city_en && $site->city) {
-        $result = $site->requestGeocode($site->city . ', ' . $site->country);
-        // $result[0]->address_components;
-        $str = $result->results[0]->address_components[0]->long_name;
-        echo "Updating english city name => ", $str , "\n";
-        $site->update(['city_en' => $str]);
-    }
-
-}
-
-// print_r($siteDetails);
+$dataSource = new TaiwanEPADataSource;
+$dataSource->updateStationDetails();
