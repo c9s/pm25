1886e69e42960e2df6e1af19efd18d8f7018faf2
c9s <yoanlin93@gmail.com>
2015-03-22 11:16:23 +0800

    Add update script

---
+++ scripts/update.php
@@ -0,0 +1,18 @@
+<?php
+require 'main.php';
+use PM25\Model\Station;
+use PM25\DataSource\Asia\TaiwanEPADataSource;
+use PM25\DataSource\Asia\JapanSoramameDataSource;
+use CLIFramework\Logger;
+use LazyRecord\ConnectionManager;
+use CurlKit\CurlAgent;
+$logger = Logger::getInstance();
+$agent = new CurlAgent;
+$dataSources = [
+    new TaiwanEPADataSource($agent, $logger),
+    new JapanSoramameDataSource($agent, $logger),
+];
+foreach($dataSources as $dataSource) {
+    $dataSource->updateStationDetails();
+    $dataSource->updateMeasurements();
+}
