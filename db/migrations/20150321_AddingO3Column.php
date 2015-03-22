334985edc583cf1eb93adfec0b3a48f389357256
c9s <yoanlin93@gmail.com>
2015-03-22 12:23:50 +0800

    Migration script: Adding O3 column

---
+++ db/migrations/20150321_AddingO3Column.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * To upgrade with new schema:
+ *    $this->importSchema(new TestApp\AuthorBookSchema);
+ *
+ * To rename table column:
+ *    $this->renameColumn($table, $columnName, $newColumnName);
+ *
+ * To create index:
+ *    $this->createIndex($table,$indexName,$columnNames);
+ *
+ * To drop index:
+ *    $this->dropIndex($table,$indexName);
+ *
+ * To add a foreign key:
+ *    $this->addForeignKey($table,$columnName,$referenceTable,$referenceColumn = null) 
+ *
+ * To drop table:
+ *    $this->dropTable('authors');
+ */
+
+class AddingO3Column_1426950661  extends LazyRecord\Migration\Migration {
+
+    public function upgrade() {
+        $this->executeSql('ALTER TABLE stations ADD COLUMN `o3` double default 0');
+    }
+
+    public function downgrade() {
+    
+}
+
+}
