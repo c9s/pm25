ed398cda1035c80cb9a3e67b82c09090c4a7e0c1
c9s <cornelius.howl@gmail.com>
2015-03-23 19:45:48 +0800

    apply data source name to update script

--- scripts/update.php
+++ scripts/update.php
@@ -9,11 +9,27 @@ use LazyRecord\ConnectionManager;
 use CurlKit\CurlAgent;
 $logger = Logger::getInstance();
 $agent = new CurlAgent;
-$dataSources = [
-    new TaiwanEPADataSource($agent, $logger),
-    new JapanSoramameDataSource($agent, $logger),
-    new ChinaAqiStudyDataSource($agent, $logger),
-];
+
+$dataSources = [];
+if (count($argv) > 1) {
+    array_shift($argv);
+
+    foreach($argv as $arg) {
+        if ($arg == "china") {
+            $dataSources[] = new ChinaAqiStudyDataSource($agent, $logger);
+        } else if ($arg == "japan") {
+            $dataSources[] = new JapanSoramameDataSource($agent, $logger);
+        } else if ($arg == "taiwan") {
+            $dataSources[] = new TaiwanEPADataSource($agent, $logger);
+        }
+    }
+} else {
+    $dataSources = [
+        new TaiwanEPADataSource($agent, $logger),
+        new JapanSoramameDataSource($agent, $logger),
+        new ChinaAqiStudyDataSource($agent, $logger),
+    ];
+}
 foreach($dataSources as $dataSource) {
     // $dataSource->updateStationDetails();
 }
