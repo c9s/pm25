1a4fc14ce6bf519d7bc51188678355f0385a4d34
c9s <yoanlin93@gmail.com>
2015-03-29 01:08:28 +0800

    Apply country condition

--- applications/PM25/Controller/StationListController.php
+++ applications/PM25/Controller/StationListController.php
@@ -2,35 +2,48 @@
 namespace PM25\Controller;
 use Phifty\Controller;
 use PM25\Model\StationCollection;
+use PM25\Predicate;
 use LazyRecord\ConnectionManager;
 
 class StationListController extends Controller
 {
     public function indexAction() {
+        $country = $this->request->param('country');
         $limit = $this->request->param('limit');
 
         $cacheKey = "pm25-station-list-$limit";
+
+        /*
         if ($json = apc_fetch($cacheKey)) {
             header('Content-Type: application/json');
             return $json;
         }
+        */
 
+        $args = [];
         $conns = ConnectionManager::getInstance();
         $conn = $conns->get('default');
-        $sql = "SELECT id, country, country_en, city, city_en, name, name_en, address, address_en, latitude, longitude 
-            FROM stations s 
-            WHERE country_en = 'Taiwan' OR country_en = 'China'
-            ORDER BY country ASC, city ASC, name ASC, id DESC";
+        $sql = "SELECT id, country, country_en, city, city_en, name, name_en, address, address_en, latitude, longitude FROM stations s ";
+
+        if ($country) {
+            $sql .= " WHERE country_en = :country "; // skip Japan because it will be too large
+            $args[":country"] = $country;
+        } else {
+            $sql .= " WHERE country_en = 'Taiwan' OR country_en = 'China'"; // skip Japan because it will be too large
+        }
+        $sql .= " ORDER BY country ASC, city ASC, name ASC, id DESC";
+
         if ($limit) {
             $sql .= ' LIMIT ' . intval($limit);
         }
 
-        $stmt = $conn->prepareAndExecute($sql);
+        $stmt = $conn->prepareAndExecute($sql, $args);
         $rows = $stmt->fetchAll();
         foreach($rows as &$row) {
             $row['id'] = intval($row['id']);
             $row['longitude'] = doubleval($row['longitude']);
             $row['latitude'] = doubleval($row['latitude']);
+            // $row = array_filter($row);
         }
         $json = $this->toJson($rows);
         apc_store($cacheKey, $json, 60 * 60 * 24);
