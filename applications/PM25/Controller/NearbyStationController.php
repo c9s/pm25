9b348df6a38fb7c9794d10cdb2962d1b14a993d5
c9s <cornelius.howl@gmail.com>
2015-04-01 16:20:36 +0800

    Improve nearby station search

--- applications/PM25/Controller/NearbyStationController.php
+++ applications/PM25/Controller/NearbyStationController.php
@@ -12,32 +12,24 @@ class NearbyStationController extends Controller
         // print_r([ 'latitude' => $latitude, 'longitude' => $longitude ]);
         $conns = ConnectionManager::getInstance();
         $conn = $conns->get('default');
+
+        // select DISTANCE(s.latitude, s.longitude, 35.702069, 121.775327) from stations s
         $stmt = $conn->prepareAndExecute("
-            SELECT s.id, s.country, s.city, s.name, s.country_en, s.city_en, s.name_en, s.latitude, s.longitude, m.pm25, m.pm10, m.aqi, m.psi,
-            111.045 * DEGREES(
-                   ACOS(COS(RADIANS(:lat))
-                 * COS(RADIANS(latitude))
-                 * COS(RADIANS(:lon) - RADIANS(longitude))
-                 + SIN(RADIANS(:lat)) * SIN(RADIANS(latitude)))) AS distance_km 
+            SELECT s.id, s.country, s.country_en, s.city, s.city_en, s.name, s.name_en, s.address, s.address_en, s.country, s.country_en, s.latitude, s.longitude,
+                 DISTANCE(s.latitude, s.longitude, :lat, :lon) as distance_km
                  FROM stations s
-                 LEFT JOIN (SELECT pm25, pm10, aqi, psi, station_id FROM measures s ORDER BY published_at DESC LIMIT 1) m ON (m.station_id = s.id)
                  WHERE latitude != 0 AND longitude != 0
                  ORDER BY distance_km ASC LIMIT $limit
                  ", 
                  [ 
                      ':lat' => doubleval($latitude),
                      ':lon' => doubleval($longitude),
-                     ':limit' => intval($limit),
                  ]);
         $rows = $stmt->fetchAll();
         foreach($rows as &$row) {
             $row['id'] = intval($row['id']);
             $row['latitude'] = doubleval($row['latitude']);
             $row['longitude'] = doubleval($row['longitude']);
-            $row['pm25'] = doubleval($row['pm25']);
-            $row['pm10'] = doubleval($row['pm10']);
-            $row['aqi'] = doubleval($row['aqi']);
-            $row['psi'] = doubleval($row['psi']);
             $row['distance_km'] = doubleval($row['distance_km']);
         }
         return $this->toJson($rows);
